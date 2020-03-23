bb80eb5064bf53f814e9cde8df747cde6ea9c0d2
Colby Garland <colby@teleoinc.com>
2020-03-23 16:24:41 -0600

    Added google ads

--- components/Header.js
+++ components/Header.js
@@ -16,6 +16,8 @@ const Header = () => (
 
                 gtag('config', 'UA-161672208-1');`}} />
 
+            <script data-ad-client="ca-pub-6415703262492824" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+
         </Head>
 
         <header>
