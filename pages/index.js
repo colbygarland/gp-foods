380a44a013d1bcd4f455311782748752ae37716d
Colby Garland <colby@teleoinc.com>
2020-03-23 14:39:41 -0600

    Added form

--- pages/index.js
+++ pages/index.js
@@ -18,6 +18,8 @@ const Home = () => (
                       <Column md={6}>
                           <h4>What is GP Foods?</h4>
                           <p>In response to COVID-19, I thought having a website with all the places in GP still delivering food / pickup was a good idea.</p>
+                          <p>DISCLAIMER: I am not affiliated with anybody, I just want to help. I am also staying home and social distancing, which you should too.</p>
+                          <p>Add a restaurant or update an existing one here: <a href={"https://forms.gle/D2rWrRuF1ZYLgDMp8"} target={"_blank"} class={"btn btn-primary"}>Update Form</a></p>
                           <p>Tap on a restaurant to call them.</p>
 
                           <Legend/>
