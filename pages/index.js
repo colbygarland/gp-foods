606b53f341cb8358f2768b5ba705de945280526f
Colby Garland <colby@teleoinc.com>
2020-03-23 12:50:53 -0600

    removed form for now

--- pages/index.js
+++ pages/index.js
@@ -19,7 +19,7 @@ const Home = () => (
                           <h4>What is GP Foods?</h4>
                           <p>In response to COVID-19, I thought having a website with all the places in GP still delivering food / pickup was a good idea.</p>
                           <p>Tap on a restaurant to call them.</p>
-                          <p><a href={"#form"}>Click here to submit a change.</a></p>
+
                           <Legend/>
                       </Column>
                       <Column md={6}>
@@ -29,12 +29,7 @@ const Home = () => (
 
               </Container>
           </Content>
-          <Content>
-              <Container>
-                  <h4 id={"form"}>What Am I Missing? What has changed?</h4>
-                  <Form/>
-              </Container>
-          </Content>
+
       </Layout>
 
   </div>
