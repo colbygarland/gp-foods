cfaf0666362c30546077feaecafbcc8906418c9b
Colby Garland <colby@teleoinc.com>
2020-03-23 11:39:45 -0600

    Added google analytics

--- components/Header.js
+++ components/Header.js
@@ -6,6 +6,15 @@ const Header = () => (
             <title>GP Foods</title>
             <link rel="icon" href="/favicon.ico" />
             <link href="https://fonts.googleapis.com/css?family=Ubuntu:300,400,500&display=swap" rel="stylesheet" />
+
+
+            <script async src="https://www.googletagmanager.com/gtag/js?id=UA-161672208-1"></script>
+            <script dangerouslySetInnerHTML={{__html: `window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+
+                gtag('config', 'UA-161672208-1');`}} />
+
         </Head>
 
         <header>
