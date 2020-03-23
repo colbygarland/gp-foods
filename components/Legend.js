fb83398ccce9fa6e0ec41973dbd7d5bbc208b567
Colby Garland <colby@teleoinc.com>
2020-03-23 12:22:54 -0600

    Added temporary

--- components/Legend.js
+++ components/Legend.js
@@ -2,7 +2,7 @@ import LegendItem from "./LegendItem";
 
 const Legend = (props) => (
     <div>
-        <LegendItem status={"danger"}>Closed</LegendItem>
+        <LegendItem status={"danger"}>Temporarily Closed</LegendItem>
         <LegendItem status={"primary"}>Delivery / Pickup / Curbside</LegendItem>
         <LegendItem status={"warning"}>Delivery Only</LegendItem>
         <LegendItem status={"success"}>Pickup Only</LegendItem>
